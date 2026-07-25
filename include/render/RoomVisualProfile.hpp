60d8e73354707300cf24fdeef38cd4c65d6e560b
Grzegorz <grzegorz2047@gmail.com>
2026-07-25 19:21:00 +0200

    visuals: expose resident placement per room silhouette

--- include/render/RoomVisualProfile.hpp
+++ include/render/RoomVisualProfile.hpp
@@ -21,16 +21,17 @@ struct RoomVisualProfile {
     float dominant_width;
     float dominant_height;
     float resident_clearance_width;
+    float resident_clear_x;
     bool uses_translucent_glow;
 };
 
 constexpr std::array<RoomVisualProfile, 6> kRoomVisualProfiles{{
-    {DominantProp::Generator, 3, 58.0f, 42.0f, 31.0f, true},
-    {DominantProp::Planter, 4, 104.0f, 28.0f, 24.0f, true},
-    {DominantProp::WaterTank, 3, 72.0f, 43.0f, 26.0f, true},
-    {DominantProp::Workbench, 4, 92.0f, 31.0f, 28.0f, true},
-    {DominantProp::StorageRack, 5, 83.0f, 45.0f, 22.0f, false},
-    {DominantProp::BunkBed, 4, 76.0f, 36.0f, 30.0f, true},
+    {DominantProp::Generator, 5, 58.0f, 42.0f, 31.0f, 101.0f, true},
+    {DominantProp::Planter, 5, 104.0f, 28.0f, 24.0f, 91.0f, true},
+    {DominantProp::WaterTank, 5, 72.0f, 43.0f, 26.0f, 12.0f, true},
+    {DominantProp::Workbench, 5, 92.0f, 31.0f, 28.0f, 62.0f, true},
+    {DominantProp::StorageRack, 5, 83.0f, 45.0f, 22.0f, 8.0f, false},
+    {DominantProp::BunkBed, 5, 76.0f, 36.0f, 30.0f, 93.0f, true},
 }};
 
 [[nodiscard]] constexpr const RoomVisualProfile& room_visual_profile(
