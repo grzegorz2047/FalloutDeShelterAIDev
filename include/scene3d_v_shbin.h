d7dd41fc6780818e0e5b9d2c88410a9926ca2843
Grzegorz <grzegorz2047@gmail.com>
2026-07-25 07:22:01 +0200

    build: adapt generated shader bytes to DVLB API

--- include/scene3d_v_shbin.h
+++ include/scene3d_v_shbin.h
@@ -3,5 +3,5 @@
 // Compatibility alias for devkitARM's generated header name for scene3d.v.pica.
 #include "scene3d_shbin.h"
 
-#define scene3d_v_shbin scene3d_shbin
+#define scene3d_v_shbin const_cast<unsigned char*>(scene3d_shbin)
 #define scene3d_v_shbin_size scene3d_shbin_size
